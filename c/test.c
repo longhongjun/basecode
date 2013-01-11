c078bfd5b1ba3ea6155fd7c60c53114e0fbdaa88
longhongjun <longhongjun@gmail.com>
2013-01-11 18:57:02 +0800

    123

---
+++ c/test.c
@@ -0,0 +1,16 @@
+/*
+ * test.c
+ *
+ *  Created on: 2013-1-10
+ *      Author: long
+ */
+#include <stdio.h>
+
+main(){
+  printf("123123");
+printf("123");
+}
+
+
+
+
---
+++ c/test.h
@@ -0,0 +1,13 @@
+/*
+ * test.h
+ *
+ *  Created on: 2013-1-10
+ *      Author: long
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+
+
+#endif /* TEST_H_ */
